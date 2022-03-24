2f636562d8b11b5507e1d7866776dca61916fac7
glennmca-dev <glennmca.dev@gmail.com>
2022-03-24 21:50:29 +0000

    lesson 16 finished

--- react/catch-of-the-day/src/components/App.js
+++ react/catch-of-the-day/src/components/App.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import { object } from "prop-types";
 
 class App extends React.Component {
   state = {
@@ -25,11 +27,30 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = (key) => {
+    //copy state
+    const order = { ...this.state.order };
+    //add to order or update number in order
+    order[key] = order[key] + 1 || 1;
+    //set state update order
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory
