8f0adaf4bccdadba8d96b1b44423110d9efec717
glennmca-dev <glennmca.dev@gmail.com>
2022-03-29 23:53:59 +0100

    lesson 22 finished, css working for order component

--- react/catch-of-the-day/src/components/App.js
+++ react/catch-of-the-day/src/components/App.js
@@ -51,6 +51,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = (key) => {
+    //copy state
+    const fishes = { ...this.state.fishes };
+    //update state
+    fishes[key] = null;
+    //update state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -61,6 +70,15 @@ class App extends React.Component {
     //add to order or update number in order
     order[key] = order[key] + 1 || 1;
     //set state update order
+    this.setState({ order });
+  };
+
+  removeFromOrder = (key) => {
+    //copy state
+    const order = { ...this.state.order };
+    //remove item
+    delete order[key];
+    //set state
     this.setState({ order });
   };
 
@@ -80,11 +98,16 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           updateFish={this.updateFish}
           fishes={this.state.fishes}
           addFish={this.addFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
         />
       </div>
